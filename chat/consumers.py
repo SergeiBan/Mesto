996a349913118b387161aeefdce260120ecf3ee6
Sergei Banshchikov <s.o.banshchikov@yandex.ru>
2023-06-09 22:27:32 +0300

    Uses Redis geosearch for people selection

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,84 +1,73 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import database_sync_to_async
-from chat.models import Spot
+import redis.asyncio as redis
 
 
-COORDS = {}
+r = redis.Redis(host='127.0.0.1', port=6379, db=0, decode_responses=True)
 
 
-class CoordRecord:
-    def __init__(self, value, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
+async def flush_redis():
+    await r.flushall(asynchronous=True)
 
-
-@database_sync_to_async
-def create_spot_record(y, x, channel_name):
-    Spot.objects.update_or_create(
-        defaults={'y': y, 'x': x}, channel_name=channel_name)
-    print(Spot.objects.all())
-
-
-@database_sync_to_async
-def delete_spot_record(channel_name):
-    print('in delete')
-    print(Spot.objects.filter(channel_name=channel_name))
-    Spot.objects.filter(channel_name=channel_name).delete()
+JUST_STARTED = True
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
     def __init__(self):
+        self.y = None
+        self.x = None
+        self.nearby = None
         super().__init__()
-        self.chats = []
 
     async def connect(self):
-        # self.room_name = (
-        #     f'{self.scope["url_route"]["kwargs"]["y"]}'
-        # )
-        # self.room_group_name = f'chat_{self.room_name}'
-
-        # await self.channel_layer.group_add(
-        #     self.room_group_name, self.channel_name
-        # )
+        global JUST_STARTED
+        if JUST_STARTED:
+            await flush_redis()
+            JUST_STARTED = False
         await self.accept()
 
     async def disconnect(self, code):
-        if self.chats:
-            for chat in self.chats:
-                await self.channel_layer.group_discard(
-                    chat, self.channel_name
-                )
-        await delete_spot_record(self.channel_name)
+        await r.close()
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
         if 'travel' in message:
-            for chat in self.chats:
-                await self.channel_layer.group_discard(
-                    chat, self.channel_name
-                )
-            self.chats = []
-            y, x = message["travel"]["y"], message["travel"]["x"]
-            # self.room_name = f'{message["travel"]["y"]}_{message["travel"]["x"]}'
-            self.room_name = f'{y}_{x}'
-            self.room_group_name = f'chat_{self.room_name}'
 
-            await self.channel_layer.group_add(
-                self.room_group_name, self.channel_name
-            )
-            self.chats.append(self.room_group_name)
-            await create_spot_record(x, y, self.channel_name)
+            self.y, self.x = message["travel"]["y"], message["travel"]["x"]
+
+            self.nearby = await get_nearby_people(self.y, self.x, r)
+            if self.nearby == 0:
+                return
+
+            for person in self.nearby:
+                await self.channel_layer.send(
+                    person,
+                    {'type': 'chat_message', 'message': f'y {self.y} x {self.x}'}
+                )
+            await deliver_message(self, self.nearby, f'y {self.y} x {self.x}')
+            await r.geoadd('World', (self.y, self.x, self.channel_name))
             return
 
-        await self.channel_layer.group_send(
-            self.room_group_name, {'type': 'chat_message', 'message': message}
-        )
+        self.nearby = await get_nearby_people(self.y, self.x, r)       
+        await deliver_message(self, self.nearby, message)
+        print(self.nearby, message)
 
     async def chat_message(self, event):
         message = event['message']
         await self.send(text_data=json.dumps({'message': message}))
+
+
+async def get_nearby_people(y, x, r):
+    return await r.geosearch(
+        name='World', longitude=y, latitude=x, unit='km', radius=1000)
+
+
+async def deliver_message(consumer, nearby, message):
+    for person in nearby:
+        await consumer.channel_layer.send(
+            person,
+            {'type': 'chat_message', 'message': message}
+        )
